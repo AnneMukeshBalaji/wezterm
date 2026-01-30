6d0f001f5da4740162f03d284f4ea234f8ee6104
AnneMukeshBalaji <mukimukku@gmail.com>
2026-01-30 20:46:36 +0530

    comminting

--- wezterm.lua
+++ wezterm.lua
@@ -9,16 +9,47 @@ return {
 	}),
 	font_size = 11.0,
 	window_background_opacity = 0.7,
+	colors = {
+		foreground = "#CBE0F0",
+		background = "#011423",
+		cursor_bg = "#47FF9C",
+		cursor_fg = "#011423",
+		selection_bg = "#033259",
+		selection_fg = "#CBE0F0",
+	},
 	keys = {
 		{
-			key = "c",
-			mods = "CTRL",
-			action = wezterm.action.CopyTo("Clipboard"),
+			key = "t",
+			mods = "ALT",
+			action = wezterm.action.SpawnTab("CurrentPaneDomain"),
+		},
+		-- Close current tab
+		{
+			key = "q",
+			mods = "ALT",
+			action = wezterm.action.CloseCurrentTab({ confirm = true }),
+		},
+
+		-- Navigate tabs (vim-style)
+		{
+			key = "h",
+			mods = "ALT",
+			action = wezterm.action.ActivateTabRelative(-1),
 		},
 		{
-			key = "v",
-			mods = "CTRL",
-			action = wezterm.action.PasteFrom("Clipboard"),
+			key = "j",
+			mods = "ALT",
+			action = wezterm.action.ActivateTabRelative(-1),
+		},
+		{
+			key = "l",
+			mods = "ALT",
+			action = wezterm.action.ActivateTabRelative(1),
+		},
+		{
+			key = "k",
+			mods = "ALT",
+			action = wezterm.action.ActivateTabRelative(1),
 		},
 	},
 	-- NOTE: Initial window size in columns and rows
